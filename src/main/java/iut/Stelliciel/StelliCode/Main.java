7aadc4900d19851f4829e3457fd6dbb7a1123b8a
Worde1000 <raphael.lizot@gmail.com>
2022-01-03 10:18:27 +0100

    Rajout du metier et ihm dans controleur + rajout de la lecture de Code.algo

--- src/main/java/iut/Stelliciel/StelliCode/Main.java
+++ src/main/java/iut/Stelliciel/StelliCode/Main.java
@@ -1,4 +1,13 @@
 package iut.Stelliciel.StelliCode;
 
+import iut.Stelliciel.StelliCode.metier.Interpreteur;
+
 public class Main {
+    private Interpreteur metier;
+    private CUI          ihm;
+
+    public Main() {
+        metier = new Interpreteur("");
+        ihm    = new CUI();
+    }
 }
--- src/main/java/iut/Stelliciel/StelliCode/metier/Interpreteur.java
+++ src/main/java/iut/Stelliciel/StelliCode/metier/Interpreteur.java
@@ -1,4 +1,40 @@
 package iut.Stelliciel.StelliCode.metier;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.FileInputStream;
+
 public class Interpreteur {
+
+    private ArrayList<String> fichier;
+
+    public Interpreteur(String adresseFichier) {
+
+        this.fichier = Interpreteur.lireFichier(adresseFichier);
+    }
+
+
+    public static ArrayList<String> lireFichier(String adresse) {
+        ArrayList<String> fichier = new ArrayList<String>();
+
+        try
+        {
+            Scanner sc = new Scanner ( new FileInputStream ( adresse ) );
+
+            while ( sc.hasNextLine() ) {
+                String ligne = sc.nextLine();
+                System.out.println(ligne);
+                fichier.add(ligne);
+            }
+            sc.close();
+        }
+        catch (Exception e){ e.printStackTrace(); }
+
+
+        return fichier;
+    }
+
+    public static void main(String[] args) {
+        new Interpreteur("C:\\Stelliciel\\StelliCode\\src\\main\\resources\\Code.algo");
+    }
 }
