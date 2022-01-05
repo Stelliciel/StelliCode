5e415b206506710bb0faab77744006937f3d2e3e
nnoma17 <vatresmanon17@gmail.com>
2022-01-05 15:24:21 +0100

    XML QUI FONCTIONNE !!!!

---
+++ src/main/java/iut/Stelliciel/StelliCode/metier/Couleur.java
@@ -0,0 +1,37 @@
+package iut.Stelliciel.StelliCode.metier;
+
+public class Couleur {
+    private String  nom;
+    private String  coulTxt,coulFond;
+    private int     valR,valG,valB;
+    private boolean gras;
+
+    public String  getNom     (){ return nom;      }
+    public String  getCoulTxt (){ return coulTxt;  }
+    public String  getCoulFond(){ return coulFond; }
+    public int     getValR    (){ return valR;     }
+    public int     getValG    (){ return valG;     }
+    public int     getValB    (){ return valB;     }
+    public boolean estGras    (){ return gras;     }
+
+    public void setNom     (String  nom ) { this.nom      = nom;  }
+    public void setCoulTxt (String  coul) { this.coulTxt  = coul; }
+    public void setCoulFond(String  coul) { this.coulFond = coul; }
+    public void setValR    (int     valR) { this.valR     = valR; }
+    public void setValG    (int     valG) { this.valG     = valG; }
+    public void setValB    (int     valB) { this.valB     = valB; }
+    public void setGras    (boolean gras) { this.gras     = gras; }
+
+    @Override
+    public String toString() {
+        return "Couleur{" +
+                "nom='" + nom + '\'' +
+                ", coulTxt='" + coulTxt + '\'' +
+                ", coulFond='" + coulFond + '\'' +
+                ", valR=" + valR +
+                ", valG=" + valG +
+                ", valB=" + valB +
+                ", gras=" + gras +
+                '}';
+    }
+}
