90067d22ff140d75aeda44964e2c26176989de5d
GordienGaspard <gaspard.gordien@etu.univ-lehavre.fr>
2022-01-07 13:08:38 +0100

    affichage avec scroll alpha utilisation de l'affichage

--- src/main/java/iut/Stelliciel/StelliCode/CUI/tabVariable/AfficheTab.java
+++ src/main/java/iut/Stelliciel/StelliCode/CUI/tabVariable/AfficheTab.java
@@ -15,7 +15,7 @@ public class AfficheTab {
     /** constructeur d'un tableau de variable vide
      */
     public AfficheTab(){
-        this.listeVar = new HashMap<>();
+        this.listeVar = new HashMap<>(1);
     }
 
     /**verifie si la variable est déja dans la liste si c'est le cas modifie la valeur sinon l'ajoute
@@ -23,7 +23,7 @@ public class AfficheTab {
      */
     public void maj(Variable var){
         for(String nom : listeVar.keySet()){
-            if(nom.equals(var.getNom())){this.listeVar.get(nom).setVal(var);}
+            if(nom.equals(var.getNom())){this.listeVar.get(nom).setVal(var.valToString());}
         }
         this.listeVar.put(var.getNom(), var);
     }
@@ -34,17 +34,17 @@ public class AfficheTab {
         StringBuilder sRep = new StringBuilder("     nom    |  valeur   \n");
         for(String nom : listeVar.keySet()){
             sRep.append(String.format("%-12s", CUI.adaptTxt(nom))+"|"
-                       +String.format("%-12s", CUI.adaptTxt(listeVar.get(nom).valToString())));
+                       +String.format("%-12s", CUI.adaptTxt(listeVar.get(nom).valToString()))+'\n');
         }
         System.out.println(sRep.toString());
         return sRep.toString();
     }
 
-    public String affLig(int num){
+    public String affLig(String toutesLig, int num){
         ArrayList<String> arrString = new ArrayList<>();
         arrString.add("");
         int ind = 0;
-        String toutesLig = this.toString();
+        if(num >= ind ){return "                        ";}
         for (char c:toutesLig.toCharArray()){
             if(c != '\n'){arrString.add(arrString.remove(ind) + c);}
             else{
@@ -52,7 +52,6 @@ public class AfficheTab {
                 arrString.add("");
             }
         }
-        if(num >= ind ){return "                        ";}
         return arrString.get(num);
     }
 }
