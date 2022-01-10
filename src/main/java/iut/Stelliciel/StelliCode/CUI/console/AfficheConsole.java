3d1712d58821f811c04470e34c4cbb1bfc64b203
GordienGaspard <gaspard.gordien@etu.univ-lehavre.fr>
2022-01-10 09:21:44 +0100

    ihm lié à la console

--- src/main/java/iut/Stelliciel/StelliCode/CUI/console/AfficheConsole.java
+++ src/main/java/iut/Stelliciel/StelliCode/CUI/console/AfficheConsole.java
@@ -15,18 +15,35 @@ public class AfficheConsole{
     }
 
     public void AjouterIn(String s){
-        this.arrString.add("user >>" + s);
+        this.arrString.add("utilisateur >>" + s);
     }
 
     public void AjouterOut(String s){
         this.arrString.add(s);
     }
 
-    public String toString(){
-        StringBuilder sRep = new StringBuilder();
-        for (String s : arrString) {
-            sRep.append(s).append('\n');
+    public void Ajouter(ArrayList<String> arrIn){
+        if(arrIn != null){
+            for (String s:arrIn) {
+                Ajouter(s.charAt(0),s.substring(1));
+            }
         }
-        return sRep.toString();
+    }
+
+    public String toString(){
+        if (arrString.isEmpty() || arrString ==null){ return "";}
+        else {
+            StringBuilder sRep = new StringBuilder();
+            if(arrString.size()>3) {
+                sRep.append(arrString.get(arrString.size() - 3)).append("\n");
+                sRep.append(arrString.get(arrString.size() - 2)).append("\n");
+                sRep.append(arrString.get(arrString.size() - 1)).append("\n");
+            } else {
+                for (String s:arrString) {
+                    sRep.append(s).append("\n");
+                }
+            }
+            return sRep.toString();
+        }
     }
 }
