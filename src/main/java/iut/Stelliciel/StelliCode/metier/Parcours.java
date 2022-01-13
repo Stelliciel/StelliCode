3dd1f38612f2fccf57e32c07b9569e6a5d95815f
GordienGaspard <gaspard.gordien@etu.univ-lehavre.fr>
2022-01-13 13:17:33 +0100

    javadoc de Parcours

--- src/main/java/iut/Stelliciel/StelliCode/metier/Parcours.java
+++ src/main/java/iut/Stelliciel/StelliCode/metier/Parcours.java
@@ -7,15 +7,26 @@ public class Parcours {
     private ArrayList<EtatLigne> lecteur;
     private int pointeur;
 
+    /**
+     * constructeur de parcours
+     */
     public Parcours() {
         this.pointeur = -1;
         this.lecteur  = new ArrayList<>();
     }
 
+    /**
+     * get le lecteur
+     * @return ArrayList &#60{@link EtatLigne}&#62, l'ensemble des états
+     */
     public ArrayList<EtatLigne> getLecteur() {
         return lecteur;
     }
 
+    /**
+     * ajouter un état de ligne au lecteur
+     * @param etatLigne l'{@link EtatLigne} à ajouter
+     */
     public void nouvelleEtat(EtatLigne etatLigne){
         if ( !lecteur.isEmpty() ){
             ArrayList<String> traceAlgo = derniereLigne().getTraceAlgo();
@@ -28,36 +39,54 @@ public class Parcours {
         lecteur.add(etatLigne);
     }
 
+    /**
+     * test si l'état ligne à un état ligne apres
+     * @return boolean si la ligne possede une ligne suivante
+     */
     public boolean hasNext() {
-        if ( pointeur < lecteur.size()-1 )
-            return true;
-
-        return false;
+        return pointeur < lecteur.size() - 1;
     }
 
+    /**
+     * accede au prochain Etat Ligne
+     * @return l'{@link EtatLigne} suivant
+     */
     public EtatLigne next(){
         pointeur++;
         return lecteur.get(pointeur);
     }
 
+    /**
+     * get l'Etat ligne actuel
+     * @return l'{@link EtatLigne} en cours
+     */
     public EtatLigne getEtat() {
         if ( pointeur < 0 )
             return null;
         return lecteur.get(pointeur); }
 
-
+    /**
+     * test si l'état ligne à un état ligne avant
+     * @return boolean si la ligne possede une ligne précédente
+     */
     public boolean hasPrec() {
-        if( pointeur > 0 )
-            return true;
-
-        return false;
+        return pointeur > 0;
     }
 
+    /**
+     * accede au précédent Etat Ligne
+     * @return l'{@link EtatLigne} précédent
+     */
     public EtatLigne prec() {
         pointeur--;
         return lecteur.get(pointeur);
     }
 
+    /**
+     * se déplace directement à une ligne
+     * @param numLigne int, la ligne à acceder
+     * @return l'{@link EtatLigne} de la ligne du numéro donné
+     */
     public EtatLigne seRendreA(int numLigne){
         if ( numLigne < getLecteur().get(0).getNumLigne() ) {
             pointeur = 0;
@@ -81,12 +110,20 @@ public class Parcours {
         return getEtat();
     }
 
+    /**
+     * get l'état ligne de la derniere ligne
+     * @return l'{@link EtatLigne} de la derniere ligne
+     */
     public EtatLigne derniereLigne(){
         if ( lecteur.isEmpty() )
             return null;
         return lecteur.get( lecteur.size() -1 );
     }
 
+    /**
+     * réinisialise à partir d'un "lire"
+     * @param e l'{@link EtatLigne} de la ligne avec le "lire"
+     */
     public void reecrire(EtatLigne e) {
 
         ArrayList<EtatLigne> nouvLecteur = new ArrayList<>();
